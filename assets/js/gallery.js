bee39800ccf042eb7a39c78fe2299efc8e7e18c3
CodeOrbital <Get101me@gmail.com>
2026-08-28 19:59:59 +0300

    Update gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -8,5 +8,22 @@ function showimg(){
   let srcr= choice.slice(1);
   let cap = src0 + srcr;
   caption.textContent = cap;
-  img.alt='cap';
+  img.alt= cap + 'logo';
+}
+let imgs= document.querySelectorAll('#imgsBox > img');
+let iCount=imgs.length;
+for(let i = 0; i < iCount; i++){
+  imgs[i].addEventListener('click',
+    function (){
+      let cSrc= imgs[i].src;
+      let cAlt= imgs[i].alt;
+      img.src= cSrc;
+      img.alt= cAlt;
+      let ctxt= cAlt.slice(0,-5);
+      let cl= ctxt.slice(1);
+      let cF= ctxt[0].toUpperCase();
+      let capt= cF + cl;
+      caption.textContent = capt;
+    }
+  );
 }
